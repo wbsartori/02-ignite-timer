d4258966771d58b8044983f1e3be76281527eaa7
wbsartori <wbsartori@gmail.com>
2022-11-23 13:11:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from './components/Button'
+
+export function App() {
+
+  return (
+    <>
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='danger'/>
+      <Button variant='success'/>
+    </>
+  )
+}
