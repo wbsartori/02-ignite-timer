6753e95b49f98fc722b37dc45d65d80c61c58d3a
wbsartori <wbsartori@gmail.com>
2022-12-17 21:58:00 -0300

    Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,36 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator } from './styles'
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor=''>Vou trabalhar em</label>
+          <input type='text' id='task' name='task' />
+
+          <label htmlFor=''>Durante</label>
+          <input type='number' id='duration' name='duration' />
+
+          <span>minutos.</span>
+        </FormContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type='submit'>
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
 }
