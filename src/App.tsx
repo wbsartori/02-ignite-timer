c35f8bed9c0fb1904e94c8fca7e80f9f56f880d2
wbsartori <wbsartori@gmail.com>
2022-12-17 21:03:15 -0300

    project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { ThemeProvider} from "styled-components";
-import {defaultTheme} from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
